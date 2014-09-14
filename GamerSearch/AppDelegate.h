7b03977484f43139e69a3e61006f735ea36659fe
ebaty <ebamasa1220@gmail.com>
2014-09-14 22:18:07 +0900

    first commit

---
+++ GamerSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GamerSearch
+//
+//  Created by Masaki EBATA on 2014/09/14.
+//  Copyright (c) 2014年 Masaki EBATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
