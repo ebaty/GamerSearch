364e0af7844f7f33bf778fccd0d061b0b05ea569
ebaty <ebamasa1220@gmail.com>
2014-09-24 02:48:36 +0900

    デバッグ中は広告を非表示に設定

--- GamerSearch/TabBarViewController.m
+++ GamerSearch/TabBarViewController.m
@@ -14,10 +14,12 @@
 @interface TabBarViewController () <NADViewDelegate>
 
 @property (nonatomic, retain) NADView * nadView;
+@property (nonatomic) BOOL showAdBanner;
 
 @end
 
 @implementation TabBarViewController
+
 
 - (void)viewDidLoad
 {
@@ -45,37 +47,46 @@
         vc.tabBarItem = [[UITabBarItem alloc] initWithTitle:tabTitle[i] image:iconImage tag:i];
     }
     
-    // 広告の挿入枠を確保
-    CGRect frame = self.tabBar.frame;
-    frame.origin.y = frame.origin.y - 50;
-    self.tabBar.frame = frame;
-    
-    // 広告の初期化・表示
-    CGRect mainScreen = UIScreen.mainScreen.bounds;
-    _nadView = [[NADView alloc] initWithFrame:CGRectMake(0, mainScreen.size.height-50, mainScreen.size.width, 50)];
-    [_nadView setNendID:@"5c5797e2cd1da1a1300c72ad36dcd4030ab064a5" spotID:@"238099"];
-    _nadView.delegate = self;
-    [self.view addSubview:self.nadView];
-    [_nadView load];
-    [_nadView showIndicator];
-
 #ifdef DEBUG
-    _nadView.isOutputLog = YES;
+    _showAdBanner = NO;
+#else
+    _showAdBanner = YES;
 #endif
-    
+
+    if ( _showAdBanner ) {
+        // 広告の挿入枠を確保
+        CGRect frame = self.tabBar.frame;
+        frame.origin.y = frame.origin.y - 50;
+        self.tabBar.frame = frame;
+        
+        // 広告の初期化・表示
+        CGRect mainScreen = UIScreen.mainScreen.bounds;
+        _nadView = [[NADView alloc] initWithFrame:CGRectMake(0, mainScreen.size.height-50, mainScreen.size.width, 50)];
+        [_nadView setNendID:@"5c5797e2cd1da1a1300c72ad36dcd4030ab064a5" spotID:@"238099"];
+        _nadView.delegate = self;
+        [self.view addSubview:self.nadView];
+        [_nadView load];
+        [_nadView showIndicator];
+        
+    #ifdef DEBUG
+        _nadView.isOutputLog = YES;
+    #endif
+    }
 }
 
 - (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
     
-    for ( UIView *view in self.view.subviews )
-    {
-        CGRect _rect = view.frame;
-        if ( ![view isKindOfClass:[UITabBar class]] && ![view isKindOfClass:[NADView class]] )
+    if ( _showAdBanner ) {
+        for ( UIView *view in self.view.subviews )
         {
-            _rect.size.height = _rect.size.height - 50;
-            view.frame = _rect;
+            CGRect _rect = view.frame;
+            if ( ![view isKindOfClass:[UITabBar class]] && ![view isKindOfClass:[NADView class]] )
+            {
+                _rect.size.height = _rect.size.height - 50;
+                view.frame = _rect;
+            }
         }
     }
 }
