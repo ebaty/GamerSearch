9f70d3a20d9969054363559a24bc46732da42758
ebaty <ebamasa1220@gmail.com>
2014-09-23 21:50:58 +0900

    Imageを追加・プライバシーポリシーを修正

---
+++ GamerSearch/UIView+CloseKeyboard.h
@@ -0,0 +1,13 @@
+//
+//  UIView+CloseKeyboard.h
+//  GamerSearch
+//
+//  Created by Masaki EBATA on 2014/09/23.
+//  Copyright (c) 2014年 Masaki EBATA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CloseKeyboard)
+
+@end
---
+++ GamerSearch/UIView+CloseKeyboard.m
@@ -0,0 +1,13 @@
+//
+//  UIView+CloseKeyboard.m
+//  GamerSearch
+//
+//  Created by Masaki EBATA on 2014/09/23.
+//  Copyright (c) 2014年 Masaki EBATA. All rights reserved.
+//
+
+#import "UIView+CloseKeyboard.h"
+
+@implementation UIView (CloseKeyboard)
+
+@end
